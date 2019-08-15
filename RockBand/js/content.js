808c8357aac6f5fc3301059e294ca701cec8993a
rushnightfir3 <ashyoda@gmail.com>
2019-08-15 19:33:42 -0400

    loaded json data

--- RockBand/js/content.js
+++ RockBand/js/content.js
@@ -2,3 +2,11 @@
  * Use AJAX to load the JSON and manipulate the HTML
  * https://joshbloom.github.io/dws1/data/rockband.json
 */
+let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+let xhr = new XMLHttpRequest();
+xhr.open('GET','https://joshbloom.github.io/dws1/data/rockband.json',true)
+xhr.send(null)
+xhr.onload=function(){
+let data = JSON.parse(xhr.responseText)
+console.log(data)
+}
