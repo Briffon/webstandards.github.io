5daadd7df5db23256cf965422524ac013226be38
rushnightfir3 <ashyoda@gmail.com>
2019-08-15 21:02:10 -0400

    started building content.js

--- RockBand/js/content.js
+++ RockBand/js/content.js
@@ -8,5 +8,23 @@ xhr.open('GET','https://joshbloom.github.io/dws1/data/rockband.json',true)
 xhr.send(null)
 xhr.onload=function(){
 let data = JSON.parse(xhr.responseText)
-console.log(data)
+//news 
+let newsArticles = document.querySelectorAll('#news article')
+console.log(newsArticles)
+// if(newsArticles){
+//     for(let x=0;x<newsArticles.length;x++){
+//       newsArticles[x].querySelector('img').src =data.news[x].imageUrl;
+//       newsArticles[x].querySelector('img').alt=data.news[x].title;
+//       newsArticles[x].querySelector('h3').innerHTML=data.news[x].title;
+//       newsArticles[x].querySelector('time').innerHTML=data.news[x].postDate;
+//       newsArticles[x].querySelector('time').datetime=data.news[x].postDate;
+//     }
+// }
+
+//events
+
+//memebrs
+
+//about
+console.log(newsArticles)
 }
